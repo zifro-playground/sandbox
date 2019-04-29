3b061b7d32855c631a0c145ef2e3c702b4348c12
kalle (jag) <kalle.jillheden@gmail.com>
2019-04-29 09:01:31 +0200

    Added basic world with squares and move function
    Does not compile yet due to Playground UI requires levels to function

---
+++ Assets/Scripts/ClrFunctions/MoveFunction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mellis;
+using Mellis.Core.Interfaces;
+using UnityEngine;
+
+public class MoveFunction : ClrFunction
+{
+	readonly Direction direction;
+
+	public MoveFunction(string name, Direction direction) : base(name)
+	{
+		this.direction = direction;
+	}
+
+	public override IScriptType Invoke(params IScriptType[] arguments)
+	{
+		var player = Object.FindObjectOfType<PlayerController>();
+
+		if (!player)
+		{
+			PMWrapper.RaiseError("Hittade ingen spelare.");
+		}
+
+		player.Walk(direction);
+
+		return null;
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PM;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	void Awake()
+	{
+		PMWrapper.SetCompilerFunctions(
+			new MoveFunction("gå_norr", Direction.North),
+			new MoveFunction("gå_väst", Direction.West),
+			new MoveFunction("gå_syd", Direction.South),
+			new MoveFunction("gå_öst", Direction.East)
+		);
+
+		PMWrapper.AutoSetSmartButtons();
+	}
+}
