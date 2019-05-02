961df88019f39ee92effc33ddb16b6b4a10ee997
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-02 14:27:31 +0200

    Added playground prefab & assemblyinfo, starting at v0.1.0

---
+++ Assets/Zifro.Sandbox/ZifroSandboxAssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using PM;
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
+
+namespace Zifro.Sandbox
+{
+	public class ZifroSandboxAssemblyInfo : AssemblyInfo
+	{
+		public override bool canBeEdited => true;
+	}
+}
