0bd4d05e24d5afa3fb0c4c9ad0fca7ce083893ed
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-15 16:21:24 +0200

    Added agent menu item name label update

--- Assets/Zifro.Sandbox/UI/AgentMenuItem.cs
+++ Assets/Zifro.Sandbox/UI/AgentMenuItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 using Zifro.Sandbox.Entities;
 
 namespace Zifro.Sandbox.UI
@@ -9,13 +10,17 @@ namespace Zifro.Sandbox.UI
 		[NonSerialized]
 		public Agent agent;
 
+		public Text label;
+
 		void Start()
 		{
 			Debug.Assert(AgentBank.main, $"Missing main agents bank in {name}.", this);
 			agent = AgentBank.main.GetAgent(this);
 			Debug.Assert(agent != null, $"Unable to get agent in {name}.", this);
+			Debug.Assert(label, $"{nameof(label)} is not assigned for {name}.", this);
 
 			name = $"Agent '{agent?.name ?? "#unnamed"}'";
+			label.text = agent.name;
 		}
 	}
 }
