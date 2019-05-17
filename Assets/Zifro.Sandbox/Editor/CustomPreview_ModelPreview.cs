da2f2a6613747e4b47ae4b566c29b91b18bd0263
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-17 11:05:53 +0200

    Added preview to model

---
+++ Assets/Zifro.Sandbox/Editor/CustomPreview_ModelPreview.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Zifro.Sandbox;
+
+[CustomPreview(typeof(ModelPreview))]
+public class CustomPreview_ModelPreview : ObjectPreview
+{
+	public override bool HasPreviewGUI()
+	{
+		return ((ModelPreview)target).texture;
+	}
+
+	public override void OnPreviewGUI(Rect r, GUIStyle background)
+	{
+		Texture texture = ((ModelPreview)target).texture;
+
+		if (!texture)
+		{
+			return;
+		}
+
+		EditorGUI.DrawPreviewTexture(r, texture, null, ScaleMode.ScaleToFit);
+	}
+}
