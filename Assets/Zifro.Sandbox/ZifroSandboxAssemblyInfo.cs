d750232f5a3215f742f55ddb28cf0936fc5826d9
kalle (jag) <kalle.jillheden@gmail.com>
2019-05-21 16:00:16 +0200

    Upped to version 0.7.0

--- Assets/Zifro.Sandbox/ZifroSandboxAssemblyInfo.cs
+++ Assets/Zifro.Sandbox/ZifroSandboxAssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using PM;
 
-[assembly: AssemblyVersion("0.6.2")]
-[assembly: AssemblyFileVersion("0.6.2")]
-[assembly: AssemblyInformationalVersion("0.6.2")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyFileVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
 
 namespace Zifro.Sandbox
 {
